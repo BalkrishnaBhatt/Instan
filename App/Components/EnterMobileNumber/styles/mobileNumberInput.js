a53667fe71ed90f32c826a6567d9a8158cee2ea0
nishchit14 <nd@jsbot.io>
2018-07-18 13:02:56 +0530

    MobileNumberInput component added

---
+++ App/Components/EnterMobileNumber/styles/mobileNumberInput.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  img: {
+    width: 32,
+    height: 32,
+    borderRadius: 16
+  },
+  txtInput: {
+    color: '#181818',
+    fontFamily: 'Helvetica NeueLTPro_Th',
+    fontSize: 18,
+    width: '100%'
+  },
+})
