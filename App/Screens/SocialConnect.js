a0ca8e56de9535e65220173b6bc041398e7a57fa
nishchit14 <nd@jsbot.io>
2018-07-19 12:14:02 +0530

    socialConnect screen

---
+++ App/Screens/SocialConnect.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+  TextInput,
+} from 'react-native'
+
+import _ from 'underscore'
+
+import Icon from 'react-native-vector-icons/Entypo'
+
+import styles from './styles/SocialConnect'
+
+export default class SocialConnect extends Component{
+
+}
